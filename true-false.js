11839db500e7d83eca568020e336832e1e81c9a6
tapos-ctrl <taposchandro231@gmail.com>
2021-02-02 08:42:11 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//Falsy:
+// false
+// 0
+// ""
+// undefined
+// null
+// NnN
+//Truthy:
+// '0', '' []
+
+let name = 'false';
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
